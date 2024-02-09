0df75895dbd5489db7d09006794038ee410be7fd
NT Thái <156497313+ntthai24@users.noreply.github.com>
2024-02-09 23:29:09 +0700

    Bai 7

--- Lab 2/src/Bai7.java
+++ Lab 2/src/Bai7.java
@@ -1,6 +1,7 @@
 public class Bai7 {
     public static void main(String[] args) {
         System.out.println("20 so nguyen duong dau tien la: ");
+        
         for (int i = 0; i <= 20; i++) {
             System.out.println(i);
         }
