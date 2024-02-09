aad064ccca96ba33b335db403fb37b6179491d0d
ntthai24 <nguyenhoangphithai1186@gmail.com>
2024-02-09 23:25:06 +0700

    Bai 7

---
+++ Lab 2/src/Bai7.java
@@ -0,0 +1,8 @@
+public class Bai7 {
+    public static void main(String[] args) {
+        System.out.println("20 so nguyen duong dau tien la: ");
+        for (int i = 0; i <= 20; i++) {
+            System.out.println(i);
+        }
+    }
+}
