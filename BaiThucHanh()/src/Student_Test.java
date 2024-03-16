b0edc97cae10d45bb80cf72c67bf57837c843741
ntthai24 <nguyenhoangphithai1186@gmail.com>
2024-03-16 22:32:29 +0700

    Student

--- BaiThucHanh()/src/Student_Test.java
+++ BaiThucHanh()/src/Student_Test.java
@@ -2,15 +2,9 @@ public class Student_Test {
     public static void main(String[] args) {
         Student sc = new Student();
 
-        //sc.Nhapthongtin();
-        //sc.Tinhdiemtongketmonhoc();
-        //sc.Xuatthongtin();
+        sc.Nhapthongtin();
+        sc.Tinhdiemtongketmonhoc();
+        sc.Xuatthongtin();
 
-        sc.NhapHoTen();
-        sc.MaSinhVien();
-        sc.TenMonHoc();
-        sc.Diem();
-        sc.DiemTongKet();
-        sc.Inthongtin();
     }
 }
