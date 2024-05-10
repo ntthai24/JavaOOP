c2d7350b50bcd6ba0b0f071b9bc806319187a109
ntthai24 <nguyenhoangphithai1186@gmail.com>
2024-05-10 10:55:44 +0700

    Silde 38 - 39 - 40 Chuong 4

--- BaiThucHanh()/src/NameClass.java
+++ BaiThucHanh()/src/NameClass.java
@@ -3,7 +3,7 @@ import java.util.LinkedList;
 public class NameClass {
 
     public static void main(String[] args) {
-        LinkedList<Integer> linkedList = new LinkedList<>();
+        // LinkedList<Integer> linkedList = new LinkedList<>();
         LinkedList list = new LinkedList<>();
     }
 }
