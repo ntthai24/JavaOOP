5d02105cac267b9e2ebb2418661a8aec5b0bedc3
ntthai24 <nguyenhoangphithai1186@gmail.com>
2024-05-10 10:56:56 +0700

    del

--- BaiThucHanh()/src/NameClass.java
+++
@@ -1,9 +0,0 @@
-import java.util.LinkedList;
-
-public class NameClass {
-
-    public static void main(String[] args) {
-        // LinkedList<Integer> linkedList = new LinkedList<>();
-        LinkedList list = new LinkedList<>();
-    }
-}
