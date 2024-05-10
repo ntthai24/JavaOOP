e461f29f67f0bd1b7f6cca5b866ca63dd4bfd14c
ntthai24 <nguyenhoangphithai1186@gmail.com>
2024-05-10 10:57:48 +0700

    Silde 27 Chuong 4

---
+++ BaiThucHanh()/src/NameClass.java
@@ -0,0 +1,9 @@
+import java.util.LinkedList;
+
+public class NameClass {
+
+    public static void main(String[] args) {
+        // LinkedList<Integer> linkedList = new LinkedList<>();
+        LinkedList list = new LinkedList<>();
+    }
+}
